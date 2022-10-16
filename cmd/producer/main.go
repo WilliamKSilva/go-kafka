b340140f22f8e42c50aa432264ac3f0a455809ff
William <wilz.kelvin12@gmail.com>
2022-10-16 17:23:24 -0300

    feat: :sparkles: create a function to create KafkaProducer and a function to Publish a Kafka message

--- cmd/producer/main.go
+++ cmd/producer/main.go
@@ -1,7 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"log"
+
+	"github.com/confluentinc/confluent-kafka-go/kafka"
+)
 
 func main() {
-	fmt.Println("Hello go!")
+	producer := NewKafkaProducer()
+	Publish("mensagem", "teste", producer, nil)
+	producer.Flush(1000)
+}
+
+func NewKafkaProducer() *kafka.Producer {
+	configmap := &kafka.ConfigMap{
+		"bootstrap.servers": "go-kafka_kafka_1:9092",
+	}
+
+	p, err := kafka.NewProducer(configmap)
+	if err != nil {
+		log.Println(err)
+	}
+
+	return p
+}
+
+func Publish(message string, topic string, producer *kafka.Producer, key []byte) error {
+	msg := &kafka.Message{
+		Value:          []byte(message),
+		TopicPartition: kafka.TopicPartition{Topic: &topic, Partition: kafka.PartitionAny},
+		Key:            key,
+	}
+
+	err := producer.Produce(msg, nil)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/WilliamKSilva/go-kafka
 
 go 1.16
+
+require github.com/confluentinc/confluent-kafka-go v1.9.2
