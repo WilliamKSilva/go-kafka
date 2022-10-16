59c07b2aba91d834b890c74d65e5af06d9b6ef51
William <wilz.kelvin12@gmail.com>
2022-10-16 18:48:01 -0300

    feat: :sparkles: add DeliveryReport function to get the event of the sent message

--- cmd/producer/main.go
+++ cmd/producer/main.go
@@ -1,20 +1,36 @@
 package main
 
 import (
+	"fmt"
 	"log"
 
 	"github.com/confluentinc/confluent-kafka-go/kafka"
 )
 
 func main() {
+	deliveryChan := make(chan kafka.Event)
 	producer := NewKafkaProducer()
-	Publish("mensagem", "teste", producer, nil)
-	producer.Flush(1000)
+	Publish("mensagem", "teste", producer, nil, deliveryChan)
+	go DeliveryReport(deliveryChan)
+
+	producer.Flush(2000)
+
+	// e := <-deliveryChan
+	// msg := e.(*kafka.Message)
+
+	// if msg.TopicPartition.Error != nil {
+	// 	fmt.Println("Erro ao enviar!")
+	// } else {
+	// 	fmt.Println("Mensagem enviada:", msg.TopicPartition)
+	// }
 }
 
 func NewKafkaProducer() *kafka.Producer {
 	configmap := &kafka.ConfigMap{
-		"bootstrap.servers": "go-kafka_kafka_1:9092",
+		"bootstrap.servers":   "go-kafka_kafka_1:9092",
+		"delivery.timeout.ms": "0",
+		"acks":                "all",
+		"enable.idempotence":  "true",
 	}
 
 	p, err := kafka.NewProducer(configmap)
@@ -25,18 +41,33 @@ func NewKafkaProducer() *kafka.Producer {
 	return p
 }
 
-func Publish(message string, topic string, producer *kafka.Producer, key []byte) error {
+func Publish(message string, topic string, producer *kafka.Producer, key []byte, deliveryChan chan kafka.Event) error {
 	msg := &kafka.Message{
 		Value:          []byte(message),
 		TopicPartition: kafka.TopicPartition{Topic: &topic, Partition: kafka.PartitionAny},
 		Key:            key,
 	}
 
-	err := producer.Produce(msg, nil)
+	err := producer.Produce(msg, deliveryChan)
 
 	if err != nil {
 		return err
 	}
 
 	return nil
+}
+
+func DeliveryReport(deliveryChan chan kafka.Event) {
+	for e := range deliveryChan {
+		switch ev := e.(type) {
+		case *kafka.Message:
+			if ev.TopicPartition.Error != nil {
+				fmt.Println("Erro ao enviar")
+			} else {
+				fmt.Println("Mensagem enviada:", ev.TopicPartition)
+				// anotar no banco de dados que a mensagem foi processado.
+				// ex: confirma que uma transferencia bancaria ocorreu.
+			}
+		}
+	}
 }
